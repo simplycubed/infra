9e79fd43fced8446962391d3e8f968ee1dfbed97
hanzala <muhammadhanzala12@gmail.com>
2021-11-09 11:30:55 +0500

    Comment out dns changes

--- dns.tf
+++ dns.tf
@@ -1,8 +1,8 @@
 
-module "dns" {
-  source     = "terraform-google-modules/cloud-dns/google"
-  type       = "public"
-  project_id = var.project_id
-  name       = "builder-dns"
-  domain     = "${var.base_domain}."
-}
+# module "dns" {
+#   source     = "terraform-google-modules/cloud-dns/google"
+#   type       = "public"
+#   project_id = var.project_id
+#   name       = "builder-dns"
+#   domain     = "${var.base_domain}."
+# }
