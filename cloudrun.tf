db9be08dbfc33c4f89665ce51d3d6e402b9320a7
hanzala <muhammadhanzala12@gmail.com>
2021-11-10 11:27:33 +0500

    Fix string

--- cloudrun.tf
+++ cloudrun.tf
@@ -3,6 +3,6 @@ module frontend {
 
   # Required parameters
   name = "frontend"
-  image = "gcr.io/"+var.project_id+"/"+var.frontend_image
+  image = "gcr.io/${var.project_id}/${var.frontend_image}"
   location = var.region
 }
