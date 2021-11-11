19a85bf7e876aa6995d7ca6540ba061ea2699379
hanzala <muhammadhanzala12@gmail.com>
2021-11-11 11:23:43 +0500

    Add custom domain mapping

--- cloudrun.tf
+++ cloudrun.tf
@@ -7,6 +7,19 @@ module frontend {
   location = var.region
 }
 
+resource "google_cloud_run_domain_mapping" "default" {
+  location = var.region
+  name     = "www.${var.base_domain}"
+
+  metadata {
+    namespace = var.project_id
+  }
+
+  spec {
+    route_name = module.frontend.id
+  }
+}
+
 module api {
   source = "garbetjie/cloud-run/google"
 
