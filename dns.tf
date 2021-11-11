6446551fd1b29ca91a7ec7834cbeedc1739b0cd9
hanzala <muhammadhanzala12@gmail.com>
2021-11-11 11:39:46 +0500

    Add dns record

--- dns.tf
+++ dns.tf
@@ -5,4 +5,14 @@ module "dns" {
   project_id = var.project_id
   name       = "builder-dns"
   domain     = "${var.base_domain}."
+  recordsets = [
+    {
+      name = "www"
+      type = "CNAME"
+      ttl  = 60
+      records = [
+        "ghs.googlehosted.com.",
+      ]
+    },
+  ]
 }
