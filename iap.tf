a703c37a09871ce38cf8420cb0dd4cfbcd4fa480
hanzala <muhammadhanzala12@gmail.com>
2021-11-20 17:38:17 +0500

    remove IAP

--- iap.tf
+++ iap.tf
@@ -1,18 +1,12 @@
-resource "google_iap_web_iam_member" "access_iap_policy" {
-  provider = google-beta
-  project  = var.project_id
-  role     = "roles/iap.httpsResourceAccessor"
-  member   = "domain:${var.iap_domain}"
-}
+# resource "google_iap_web_iam_member" "access_iap_policy" {
+#   provider = google-beta
+#   project  = var.project_id
+#   role     = "roles/iap.httpsResourceAccessor"
+#   member   = "domain:${var.iap_domain}"
+# }
 
-resource "google_iap_brand" "project_brand" {
-  support_email     = "support@simplycubed.com"
-  application_title = "Cloud IAP protected Application"
-  project           = var.project_id
-}
-
-resource "google_iap_client" "iap_client" {
-  provider     = google-beta
-  display_name = "IAP Auth"
-  brand        = google_iap_brand.project_brand.name
-}
+# resource "google_iap_client" "iap_client" {
+#   provider     = google-beta
+#   display_name = "IAP Auth"
+#   brand        = var.iap_brand_name
+# }
