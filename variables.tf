47958c7cceceb3d0c5c56b3eb0c36083b8f3f46c
Charles Green <cg@simplycubed.com>
2021-12-07 22:15:45 +0900

    Update builder web secrets

--- variables.tf
+++ variables.tf
@@ -87,11 +87,11 @@ variable "project_id" {
 variable "region" {
   description = "Region for gcloud resources"
 }
-variable "source_graph_client_id"{
+variable "source_graph_client_id" {
   description = "source graph client id"
 }
 
-variable "source_graph_client_secret"{
+variable "source_graph_client_secret" {
   description = "source graph client id"
 }
 
