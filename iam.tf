b5a385649aeb9aea4aefbede54f5d9e9316132e7
Charles Green <cg@simplycubed.com>
2021-12-23 23:13:27 +0900

    Add firebase service account and create access key

--- iam.tf
+++ iam.tf
@@ -5,3 +5,27 @@ resource "google_project_iam_member" "project" {
   role    = "roles/secretmanager.secretAccessor"
   member  = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
 }
+
+resource "google_service_account" "firebase" {
+  project      = var.project_id
+  account_id   = "firebase"
+  display_name = "firebase"
+}
+
+resource "google_project_iam_member" "firebase" {
+  role   = "roles/firebase.viewer"
+  member = "serviceAccount:${google_service_account.firebase.email}"
+}
+
+resource "google_service_account_key" "firebase" {
+  service_account_id = google_service_account.firebase.name
+}
+
+resource "kubernetes_secret" "firebase" {
+  metadata {
+    name = "firebase"
+  }
+  data = {
+    "firebase.json" = base64decode(google_service_account_key.firebase.private_key)
+  }
+}
