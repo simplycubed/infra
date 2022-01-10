1b44c5327f3dba14e55f749588bfc165f4211826
Charles Green <cg@simplycubed.com>
2022-01-10 13:29:27 +0900

    remove unauthenticated pulibc access

--- cloudrun.tf
+++ cloudrun.tf
@@ -1,8 +1,10 @@
 module "api" {
-  source   = "garbetjie/cloud-run/google"
-  name     = "api"
-  image    = "gcr.io/${var.project_id}/${var.builder_api_image}"
-  location = var.region
+  source              = "garbetjie/cloud-run/google"
+  name                = "api"
+  image               = "gcr.io/${var.project_id}/${var.builder_api_image}"
+  location            = var.region
+  allow_public_access = true
+  http2               = true
   env = [
     { key = "ENV", value = "dev" },
     { key = "ALLOWED_ORIGIN", value = "*" },
@@ -28,10 +30,12 @@ resource "google_cloud_run_domain_mapping" "api" {
 }
 
 module "registry" {
-  source   = "garbetjie/cloud-run/google"
-  name     = "registry"
-  image    = "gcr.io/${var.project_id}/${var.registry_api_image}"
-  location = var.region
+  source              = "garbetjie/cloud-run/google"
+  name                = "registry"
+  image               = "gcr.io/${var.project_id}/${var.registry_api_image}"
+  location            = var.region
+  allow_public_access = true
+  http2               = true
   env = [
     { key = "ENV", value = "dev" },
     { key = "ALLOWED_ORIGIN", value = "*" },
