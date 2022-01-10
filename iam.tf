3ea0166d7443d50f62a5faa42c515f3ce4ee71d7
Charles Green <cg@simplycubed.com>
2022-01-10 13:40:16 +0900

    Give compute default service IAM permissions to access secrets

--- iam.tf
+++ iam.tf
@@ -6,6 +6,12 @@ resource "google_project_iam_member" "project" {
   member  = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
 }
 
+resource "google_project_iam_member" "compute" {
+  project = var.project_id
+  role    = "roles/secretmanager.secretAccessor"
+  member  = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+}
+
 resource "google_service_account" "firebase" {
   project      = var.project_id
   account_id   = "firebase"
