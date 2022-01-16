0838962f029f5dac0b7015254e84ecf5e0db52d3
Charles Green <cg@simplycubed.com>
2022-01-16 09:41:56 +0900

    Comment firebase

--- firebase.tf
+++ firebase.tf
@@ -1,73 +1,73 @@
-# Shared
-resource "google_firebase_project" "default" {
-  provider = google-beta
-  project  = data.google_project.project.project_id
-}
+# # Shared
+# resource "google_firebase_project" "default" {
+#   provider = google-beta
+#   project  = data.google_project.project.project_id
+# }
 
-# simplycubed-builder-${var.env}.appspot.com was automatically created
-# Adding a new bucket due to domain verification error in TF
-resource "google_storage_bucket" "default" {
-  provider = google-beta
-  name     = "simplycubed-builder-${var.env}"
-  location = "US"
-}
+# # simplycubed-builder-${var.env}.appspot.com was automatically created
+# # Adding a new bucket due to domain verification error in TF
+# resource "google_storage_bucket" "default" {
+#   provider = google-beta
+#   name     = "simplycubed-builder-${var.env}"
+#   location = "US"
+# }
 
-#  Builder
-resource "google_firebase_web_app" "builder" {
-  provider     = google-beta
-  project      = data.google_project.project.project_id
-  display_name = "simplycubed-builder-${var.env}"
+# #  Builder
+# resource "google_firebase_web_app" "builder" {
+#   provider     = google-beta
+#   project      = data.google_project.project.project_id
+#   display_name = "simplycubed-builder-${var.env}"
 
-  depends_on = [google_firebase_project.default]
-}
+#   depends_on = [google_firebase_project.default]
+# }
 
-data "google_firebase_web_app_config" "builder" {
-  provider   = google-beta
-  web_app_id = google_firebase_web_app.builder.id
-}
+# data "google_firebase_web_app_config" "builder" {
+#   provider   = google-beta
+#   web_app_id = google_firebase_web_app.builder.id
+# }
 
-resource "google_storage_bucket_object" "builder" {
-  provider = google-beta
-  bucket   = google_storage_bucket.default.name
-  name     = "builder-firebase-config.json"
+# resource "google_storage_bucket_object" "builder" {
+#   provider = google-beta
+#   bucket   = google_storage_bucket.default.name
+#   name     = "builder-firebase-config.json"
 
-  content = jsonencode({
-    appId             = google_firebase_web_app.builder.app_id
-    apiKey            = data.google_firebase_web_app_config.builder.api_key
-    authDomain        = data.google_firebase_web_app_config.builder.auth_domain
-    databaseURL       = lookup(data.google_firebase_web_app_config.builder, "database_url", "")
-    storageBucket     = lookup(data.google_firebase_web_app_config.builder, "storage_bucket", "")
-    messagingSenderId = lookup(data.google_firebase_web_app_config.builder, "messaging_sender_id", "")
-    measurementId     = lookup(data.google_firebase_web_app_config.builder, "measurement_id", "")
-  })
-}
+#   content = jsonencode({
+#     appId             = google_firebase_web_app.builder.app_id
+#     apiKey            = data.google_firebase_web_app_config.builder.api_key
+#     authDomain        = data.google_firebase_web_app_config.builder.auth_domain
+#     databaseURL       = lookup(data.google_firebase_web_app_config.builder, "database_url", "")
+#     storageBucket     = lookup(data.google_firebase_web_app_config.builder, "storage_bucket", "")
+#     messagingSenderId = lookup(data.google_firebase_web_app_config.builder, "messaging_sender_id", "")
+#     measurementId     = lookup(data.google_firebase_web_app_config.builder, "measurement_id", "")
+#   })
+# }
 
-# Registry
-resource "google_firebase_web_app" "registry" {
-  provider     = google-beta
-  project      = data.google_project.project.project_id
-  display_name = "simplycubed-registry-${var.env}"
+# # Registry
+# resource "google_firebase_web_app" "registry" {
+#   provider     = google-beta
+#   project      = data.google_project.project.project_id
+#   display_name = "simplycubed-registry-${var.env}"
 
-  depends_on = [google_firebase_project.default]
-}
+#   depends_on = [google_firebase_project.default]
+# }
 
-data "google_firebase_web_app_config" "registry" {
-  provider   = google-beta
-  web_app_id = google_firebase_web_app.registry.id
-}
+# data "google_firebase_web_app_config" "registry" {
+#   provider   = google-beta
+#   web_app_id = google_firebase_web_app.registry.id
+# }
 
-resource "google_storage_bucket_object" "registry" {
-  provider = google-beta
-  bucket   = google_storage_bucket.default.name
-  name     = "registry-firebase-config.json"
+# resource "google_storage_bucket_object" "registry" {
+#   provider = google-beta
+#   bucket   = google_storage_bucket.default.name
+#   name     = "registry-firebase-config.json"
 
-  content = jsonencode({
-    appId             = google_firebase_web_app.registry.app_id
-    apiKey            = data.google_firebase_web_app_config.registry.api_key
-    authDomain        = data.google_firebase_web_app_config.registry.auth_domain
-    databaseURL       = lookup(data.google_firebase_web_app_config.registry, "database_url", "")
-    storageBucket     = lookup(data.google_firebase_web_app_config.registry, "storage_bucket", "")
-    messagingSenderId = lookup(data.google_firebase_web_app_config.registry, "messaging_sender_id", "")
-    measurementId     = lookup(data.google_firebase_web_app_config.registry, "measurement_id", "")
-  })
-}
+#   content = jsonencode({
+#     appId             = google_firebase_web_app.registry.app_id
+#     apiKey            = data.google_firebase_web_app_config.registry.api_key
+#     authDomain        = data.google_firebase_web_app_config.registry.auth_domain
+#     databaseURL       = lookup(data.google_firebase_web_app_config.registry, "database_url", "")
+#     storageBucket     = lookup(data.google_firebase_web_app_config.registry, "storage_bucket", "")
+#     messagingSenderId = lookup(data.google_firebase_web_app_config.registry, "messaging_sender_id", "")
+#     measurementId     = lookup(data.google_firebase_web_app_config.registry, "measurement_id", "")
+#   })
+# }
