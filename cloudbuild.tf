2e8d43b94f0db1dc9407577cab87525772a1ba4a
Charles Green <cg@simplycubed.com>
2022-01-27 09:01:10 +0900

    Upate cloud build pr with env vars

--- cloudbuild.tf
+++ cloudbuild.tf
@@ -148,6 +148,17 @@ resource "google_cloudbuild_trigger" "build_builder_web" {
       branch = ".*"
     }
   }
+  substitutions = {
+    _ENV                          = var.env
+    _FIREBASE_API_KEY             = var.firebase_api_key
+    _FIREBASE_APP_ID              = var.firebase_app_id
+    _FIREBASE_AUTH_DOMAIN         = var.firebase_auth_domain
+    _FIREBASE_BUILDER_URL         = var.builder_api_url
+    _FIREBASE_GITHUB_CLIENT_ID    = var.github_client_id
+    _FIREBASE_MESSAGING_SENDER_ID = var.firebase_messaging_sender_id
+    _FIREBASE_REGISTRY_URL        = var.registry_api_url
+    _FIREBASE_STORAGE_BUCKET      = var.firebase_storage_bucket
+  }
   filename = "cloudbuild.pr.yaml"
   tags     = ["managed by terraform"]
 }
