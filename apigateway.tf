8282e5a9e5b610a806919f442b2f28cab0aaa261
Charles Green <cg@simplycubed.com>
2022-01-28 17:42:59 +0900

    API gateway config

--- apigateway.tf
+++ apigateway.tf
@@ -3,14 +3,14 @@ resource "google_api_gateway_api" "api" {
   api_id   = "api"
 }
 
-resource "google_api_gateway_api_config" "registry" {
+resource "google_api_gateway_api_config" "api" {
   provider      = google-beta
   api           = google_api_gateway_api.api.api_id
   api_config_id = "cfg"
   openapi_documents {
     document {
       path     = "spec.yaml"
-      contents = filebase64("apis/${var.env}/registry.yaml")
+      contents = filebase64("apis/${var.env}/combined.yaml")
     }
   }
   lifecycle {
