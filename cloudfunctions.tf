6d6ff7b2703ad3d2cb50a266e6f363ffb27842b1
Charles Green <cg@simplycubed.com>
2022-02-09 09:06:27 +0900

    Download gcloud again

--- cloudfunctions.tf
+++ cloudfunctions.tf
@@ -11,7 +11,7 @@ module "second" {
   source                            = "terraform-google-modules/gcloud/google"
   platform                          = "linux"
   additional_components             = ["beta"]
-  skip_download                     = true
+  skip_download                     = false
   use_tf_google_credentials_env_var = true
   create_cmd_body                   = "beta functions list --project ${var.project_id} --regions=${var.region}"
   depends_on = [
