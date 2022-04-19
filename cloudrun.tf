e4b4b242291af97715c1942349b6b804c7cc9cb6
Charles Green <cg@simplycubed.com>
2022-04-20 00:51:00 +0900

    Make PSP privage

--- cloudrun.tf
+++ cloudrun.tf
@@ -5,7 +5,7 @@ module "security_policies" {
   image               = "gcr.io/${var.project_id}/${var.default_image}"
   location            = var.region
   map_domains         = ["security-policies.${var.base_domain}"]
-  allow_public_access = true
+  allow_public_access = false
   ingress             = "all"
   env = [
     { key = "ENV", value = "${var.env}" },
